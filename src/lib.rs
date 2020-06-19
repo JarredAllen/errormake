83e04aca47d65792cb1632a50a56e2110e195cfa
JarredAllen <jarredallen73@gmail.com>
2020-06-18 21:53:03 -0700

    Add option for public or private errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
-#[macro_export]
-macro_rules! errormake {
+macro_rules! errormake_impls {
     ($structname:ident) => {
-        #[derive(Debug)]
-        struct $structname {
-            source: Option<Box<dyn std::error::Error + 'static>>,
-            description: Option<String>,
-        }
-
+        #[allow(dead_code)]
         impl $structname {
             /// Create a $structname with no source or description
             pub fn new() -> $structname {
@@ -60,12 +54,37 @@ macro_rules! errormake {
     }
 }
 
+#[macro_export]
+macro_rules! errormake {
+    ($structname:ident) => {
+        #[derive(Debug)]
+        struct $structname {
+            source: Option<Box<dyn std::error::Error + 'static>>,
+            description: Option<String>,
+        }
+
+        errormake_impls!($structname);
+    };
+    (pub $structname:ident) => {
+        #[derive(Debug)]
+        pub struct $structname {
+            source: Option<Box<dyn std::error::Error + 'static>>,
+            description: Option<String>,
+        }
+
+        errormake_impls!($structname);
+    }
+}
+
+errormake!(pub ExampleErrorStruct);
+
 #[cfg(test)]
 mod tests {
     use super::errormake;
     use std::error::Error;
 
     errormake!(TestingError);
+    errormake!(pub PublicTestingError);
 
     #[test]
     fn test_stable() {
@@ -75,5 +94,11 @@ mod tests {
         let error2 = TestingError::with_description(String::from("Custom error message"));
         assert_eq!("TestingError: Custom error message", format!("{}", error2));
         assert!(error2.source().is_none());
+        let error3 = TestingError::with_source(Box::new(error2));
+        assert_eq!("TestingError: Custom error message\n\nThe above error caused the following error:\n\nTestingError: No description provided", format!("{}", error3));
+        assert!(error3.source().is_some());
+        let error4 = TestingError::with_source_and_description(Box::new(TestingError::with_description(String::from("Custom error message"))), String::from("Another message"));
+        assert_eq!("TestingError: Custom error message\n\nThe above error caused the following error:\n\nTestingError: Another message", format!("{}", error4));
+        assert!(error4.source().is_some());
     }
 }
